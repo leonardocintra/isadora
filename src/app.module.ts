1b006c465d04a92420d73c80114bf94ed08566f2
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-08-26 18:00:53 -0300

    :sparkles: consumo do sqs ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,51 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ProcessadorService } from './processador/processador.service';
+import { SqsModule } from '@ssut/nestjs-sqs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ProcessadorModule } from './processador/processador.module';
+import config from './config';
+import { SQSClient } from '@aws-sdk/client-sqs';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+    ScheduleModule.forRoot(),
+    SqsModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const accessKeyId = configService.get<string>('aws.accessKeyId');
+        const secretAccessKey = configService.get<string>(
+          'aws.secretAccessKey',
+        );
+
+        return {
+          consumers: [
+            {
+              name: configService.get<string>('aws.queue_name'),
+              queueUrl: configService.get<string>('aws.url'),
+              region: configService.get<string>('aws.region'),
+              batchSize: 10,
+              maxNumberOfMessages: 10,
+              visibilityTimeout: 30,
+              waitTimeSeconds: 20,
+              sqs: new SQSClient({
+                region: configService.get<string>('aws.region'),
+                credentials: {
+                  accessKeyId,
+                  secretAccessKey,
+                },
+              })
+            },
+          ],
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ProcessadorModule,
+  ],
   controllers: [AppController],
   providers: [AppService, ProcessadorService],
 })
--- src/processador/processador.service.ts
+++ src/processador/processador.service.ts
@@ -1,12 +1,24 @@
+import { Message } from '@aws-sdk/client-sqs';
 import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { SqsMessageHandler } from '@ssut/nestjs-sqs';
 
 @Injectable()
 export class ProcessadorService {
   private readonly logger = new Logger(ProcessadorService.name);
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  constructor(private readonly configService: ConfigService) { }
+
+  @Cron(CronExpression.EVERY_10_HOURS)
   handleCron() {
     this.logger.debug('Executando processamento...');
+  }
+
+  @SqsMessageHandler('restaurante-pedidos-do-cliente', false)
+  async handleMessage(message: Message) {
+    const msgBody: any = JSON.parse(message.Body);
+
+    console.log(`Consumindo mensagem ... - ${JSON.stringify(msgBody)}`);
   }
 }
