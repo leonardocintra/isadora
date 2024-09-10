9be80f6213fb2fc00f221c025af902f8e54f883d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-09-10 07:30:31 -0300

    :bookmark: funciona websocket

--- src/pedido/pedido.socket.ts
+++ src/pedido/pedido.socket.ts
@@ -4,34 +4,37 @@ import {
   OnGatewayInit,
   WebSocketGateway,
   WebSocketServer,
-} from '@nestjs/websockets';
-import { Server } from 'socket.io';
-import { Pedido } from './entities/pedido.entity';
+} from "@nestjs/websockets";
+import { Server } from "socket.io";
+import { Pedido } from "./entities/pedido.entity";
 
 @WebSocketGateway({
   cors: {
-    origin: "https://restaurante-adminsite.ypg4r9.easypanel.host", //"http://localhost:3000",
-    methods: ['GET', 'POST'],
-  }
+    origin: "*", //"https://restaurante-adminsite.ypg4r9.easypanel.host", //"http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 })
 export class PedidoSocket
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer() server: Server;
 
   afterInit(server: any) {
-    console.log("WebSocket - Cliente conectado")
+    console.log("WebSocket - Cliente conectado");
   }
 
   handleConnection(client: any, ...args: any[]) {
-    console.log(`WebSocket - Cliente handleConnection, ${client.id}`)
+    console.log(`WebSocket - Cliente handleConnection, ${client.id}`);
   }
 
   handleDisconnect(client: any) {
-    console.log(`WebSocket - Cliente handleDisconnect, ${client.id}`)
+    console.log(`WebSocket - Cliente handleDisconnect, ${client.id}`);
   }
 
   avisarNovoPedidoGerado(pedido: Pedido) {
-    console.log(`WebSocket - Novo pedido gerado: ${JSON.stringify(pedido.restaurante)}: ${JSON.stringify(pedido.pedidoId)}`)
-    this.server.emit('novo-pedido-gerado', pedido);
+    console.log(
+      `WebSocket - Novo pedido gerado: ${JSON.stringify(pedido.restaurante)}: ${JSON.stringify(pedido.pedidoId)}`,
+    );
+    this.server.emit("novo-pedido-gerado", pedido);
   }
 }
